0679b90e2b9a7fb57d64fd9c879b87260a28424f
mbesson <mickabesson@gmail.com>
2016-07-09 19:48:39 +0200

    Create DB, models of IHM

--- IHM/models.py
+++ IHM/models.py
@@ -2,4 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=255, blank=False, unique=True)
+
+
+class Probe(models.Model):
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    room = models.ForeignKey(Room)
+    last_update = models.DateTimeField(auto_now=True)
+
+
+class Temperature(models.Model):
+    temperature = models.DecimalField(decimal_places=2, blank=False, max_digits=6)
+    datetime = models.DateTimeField(auto_now_add=True)
+    probe = models.ForeignKey(Probe)
