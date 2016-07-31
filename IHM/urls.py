f452d7398d77a98606c85ab5d613026c438f6171
mbesson <mickabesson@gmail.com>
2016-07-31 16:19:44 +0200

    Create new template of index page

--- IHM/urls.py
+++ IHM/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from IHM import views
 
 urlpatterns = [
-        url(r'^index$',                                          view=views.index,      name='index'),
+        url(r'^$',                                          view=views.index,      name='index'),
         url(r'^index$',                                          view=views.index,      name='index'),
         url(r'^settings$',                                          view=views.settings,      name='settings'),
         url(r'^json$',                                          view=views.get_json,      name='json'),
--- IHM/views.py
+++ IHM/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render, HttpResponse
 from IHM.models import Temperature, Probe
 import subprocess
-import json
 from django.http import JsonResponse
 import calendar
 from django.utils import timezone
+import redis
+import datetime
+import time
+
 timezone.localtime(timezone.now())
 
+
 def index(request):
-    return render(request, "index.html", {})
+    r = redis.StrictRedis(host='localhost', port=6379, db=0, unix_socket_path="/tmp/redis.sock")
+    probe_temp = {}
+    probe_color = ["#aaeeee", "#90ee7e", "#2b908f"]
+    last_update = r.rpop("last_update")
+    r.rpush("last_update", last_update)
+
+    for a_probe in Probe.objects.all():
+        min = r.rpop("min_temp." + a_probe.name)
+        max = r.rpop("max_temp." + a_probe.name)
+        cur = r.rpop("cur_temp." + a_probe.name)
+        probe_temp[a_probe.label + "  (today)"] = {"Current ": cur, "Max": max, "Min": min}
+
+    print "Temp max/min probes"
+    print probe_temp
+
+    return render(request, "index.html", {
+        "probe_temps": probe_temp,
+        "probe_color": probe_color,
+        "last_update": last_update[:-7],
+    })
 
 
 def settings(request):
@@ -27,16 +50,47 @@ def settings(request):
 
 def get_json(request):
     index = 1
-
+    count = 0
     data = {}
-    #result = {"pointInterval": 60000}
-    #result["pointStart"] = calendar.timegm(Temperature.objects.earliest("datetime").datetime.timetuple()) * 1000 + 7200000
-    #result["date"] = Temperature.objects.earliest("datetime").datetime
+    start = datetime.datetime.now()
+    r = redis.StrictRedis(host='localhost', port=6379, db=0, unix_socket_path="/tmp/redis.sock")
+
     for a_probe in Probe.objects.all():
+        max_temp = None
+        min_temp = None
+        curent_temp = None
         data['data' + str(index)] = []
-        for a_value in Temperature.objects.filter(probe=a_probe).order_by("datetime"):
-            data['data' + str(index)].append([calendar.timegm(a_value.datetime.timetuple()) * 1000 + 7200000, float(a_value.temperature)])
-            #data['data' + str(index)].append(float(a_value.temperature))
+        result = r.lrange(a_probe.name, 0, -1)
+        print(datetime.datetime.now() - start)
+
+        current_month = datetime.datetime.now().month
+        cureent_day = datetime.datetime.now().day
+
+        for a_line in result:
+            line = a_line.strip()
+            fields = line.split(',')
+
+            date = datetime.datetime.fromtimestamp(int(fields[0][:-3])).timetuple()
+            if date.tm_mday == cureent_day and date.tm_mon == current_month:
+                temp_float = float(fields[1])
+                if max_temp is None:
+                    max_temp = temp_float
+                elif max_temp < temp_float:
+                    max_temp = temp_float
+
+                if min_temp is None:
+                    min_temp = temp_float
+                elif min_temp > temp_float:
+                    min_temp = temp_float
+
+                curent_temp = temp_float
+            data['data' + str(index)].append([int(fields[0]), float(fields[1])])
+            count += 1
+        data['data' + str(a_probe.name)] = ([max_temp, min_temp])
+        r.rpush("min_temp." + a_probe.name, min_temp)
+        r.rpush("max_temp." + a_probe.name, max_temp)
+        r.rpush("cur_temp." + a_probe.name, curent_temp)
+        print(datetime.datetime.now() - start)
         index += 1
-    #result["dataLength"] = count
+    data['value_nbr'] = str(count)
     return JsonResponse(data)
