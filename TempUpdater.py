98a6bfb6d29a65116c4daa8451108291e69abf3e
mbesson <mickabesson@gmail.com>
2016-07-31 16:20:29 +0200

    implement redis in TempUpdater

--- TempUpdater.py
+++ TempUpdater.py
@@ -7,6 +7,8 @@ import django
 import time
 from django.utils import timezone
 import datetime
+import calendar
+import redis
 
 # -------------------------------------------------------------------------------------------------------------------- #
 
@@ -16,11 +18,30 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AquaMonitor.settings")
 django.setup()
 
 print datetime.datetime.now()
-from IHM.models import Temperature, Probe, Room
+from IHM.models import Temperature, Probe
+
+r = redis.StrictRedis(host='localhost', port=0, db=0, unix_socket_path="/tmp/redis.sock")
+first_start = True
 
 while True:
+    if first_start is True:
+        print "Flush old data"
+        r.flushall()
+
+        print "load data from DB"
+        for a_probe in Probe.objects.all():
+            for a_value in Temperature.objects.filter(probe=a_probe).order_by("datetime"):
+                timestamp = calendar.timegm(a_value.datetime.timetuple()) * 1000
+                r.rpush(a_probe.name, str(timestamp) + ", " + str(float(a_value.temperature)))
+
+        first_start = False
+
+    print("Sleep for 150 sec.")
+    time.sleep(150)
+
     base_dir = '/sys/bus/w1/devices/'
     probes = os.listdir(base_dir)
+    print datetime.datetime.now()
     print probes
 
     for a_probe in probes:
@@ -40,7 +61,9 @@ while True:
             temperature = Temperature(temperature=temp, probe=probe, datetime=datetime.datetime.now())
             temperature.save()
 
+            r.rpush(probe.name, str(calendar.timegm(datetime.datetime.now().timetuple()) * 1000) + ", " + str(float(temp)))
+            r.rpush("last_update", str(datetime.datetime.now()))
+
         else:
             print "Not a probe"
 
-    time.sleep(60)
